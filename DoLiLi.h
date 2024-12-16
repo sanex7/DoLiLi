ba2ccb7d8367989367a769522df62f202404744a
sanex7 <numasasa2008@gmail.com>
2024-12-16 13:02:21 +0200

    Created class Double_Linked_List(DoLiLi) with basics

--- DoLiLi.h
+++ DoLiLi.h
@@ -1,1 +1,95 @@
-#pragma once
+#ifndef DOUBLY_LINKED_LIST_H
+#define DOUBLY_LINKED_LIST_H
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class DoublyLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* prev;
+        Node* next;
+        Node(T value) : data(value), prev(nullptr), next(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    ~DoublyLinkedList() {
+        DeleteAll();
+    }
+
+    void AddToHead(T value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+        }
+        else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void AddToTail(T value) {
+        Node* newNode = new Node(value);
+        if (!tail) {
+            head = tail = newNode;
+        }
+        else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void DeleteFromHead() {
+        if (!head) return;
+
+        Node* temp = head;
+        if (head == tail) {
+            head = tail = nullptr;
+        }
+        else {
+            head = head->next;
+            head->prev = nullptr;
+        }
+        delete temp;
+    }
+
+    void DeleteFromTail() {
+        if (!tail) return;
+
+        Node* temp = tail;
+        if (head == tail) {
+            head = tail = nullptr;
+        }
+        else {
+            tail = tail->prev;
+            tail->next = nullptr;
+        }
+        delete temp;
+    }
+
+    void DeleteAll() {
+        while (head) {
+            DeleteFromHead();
+        }
+    }
+
+    void Show() const {
+        Node* current = head;
+        while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+#endif
